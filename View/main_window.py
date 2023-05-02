43c03f428c2bdc2a0cb2c54e9f09de27ecf7fdb0
SzauterAbel <abel.szauter@gmail.com>
2023-05-02 16:49:44 +0200

    Created base for Tortura
    Try it :D
    
    
    Signed-off-by: SzauterAbel <abel.szauter@gmail.com>

---
+++ View/main_window.py
@@ -0,0 +1,32 @@
+from View.qt_functions import *
+from View.WelcomeWindow.welcome_window import WelcomeWindow
+from View.WelcomeWindow.new_tortura_window import NewTorturaWindow
+import sys
+from functools import partial
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setGeometry(100, 100, 400, 284)
+        self.setWindowTitle("Tortúra")
+        self.initUI()
+
+
+    def initUI(self):
+        welcome_window = WelcomeWindow()
+        new_tortura_window = NewTorturaWindow()
+
+        self.setCentralWidget(welcome_window)
+
+        welcome_window.new_tortura_signal.connect(partial(self.updateUI, new_tortura_window))
+
+    def updateUI(self, widget):
+        self.setCentralWidget(widget)
+
+def window():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+
