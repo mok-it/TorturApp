a7ed70c797fc0e3f0c1a17d43a060c4fb579d420
Oliver Raisz <raiszoliver@gmail.com>
2024-02-02 11:32:16 +0100

    added a simple unit test

---
+++ src/test/unittests/QtFunctions.py
@@ -0,0 +1,16 @@
+import unittest
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+import sys
+from src.main.view import qt_functions
+
+app = QApplication(sys.argv)
+
+class TestQtFunctions(unittest.TestCase):
+    def test_createLabel(self):
+        label = qt_functions.createLabel("Test")
+        self.assertIsInstance(label, QtWidgets.QLabel)
+        self.assertEqual(label.text(), "Test")
+
+if __name__ == '__main__':
+    unittest.main()
