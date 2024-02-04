00105a4036c9443b8000a2756691389211ad1147
SzauterAbel <abel.szauter@gmail.com>
2024-02-04 17:08:20 +0100

    Add path to the test class
    Signed-off-by: SzauterAbel <abel.szauter@gmail.com>

--- src/test/unittests/qt_functions_test.py
+++ src/test/unittests/qt_functions_test.py
@@ -1,16 +1,21 @@
 import unittest
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
-import sys
-from src.main.view import qt_functions
+import sys, os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.append(parent_dir)
+from main.view.qt_functions import create_label
 
 app = QApplication(sys.argv)
 
+
 class TestQtFunctions(unittest.TestCase):
     def test_createLabel(self):
-        label = qt_functions.create_label("Test")
+        label = create_label("Test")
         self.assertIsInstance(label, QtWidgets.QLabel)
         self.assertEqual(label.text(), "Test")
+
 
 if __name__ == '__main__':
     unittest.main()
